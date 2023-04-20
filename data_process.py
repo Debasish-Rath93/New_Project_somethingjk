b6e9e8fa64b87775d372b29574ba513e64352457
Debasish-Rath93 <erdebasishrath1993@gmail.com>
2023-04-20 18:02:06 +0530

    I have done something proper now multi and add

--- data_process.py
+++ data_process.py
@@ -1,3 +1,14 @@
 
 print("i dont know what to do?")
 print("\n whover whatever my code my world")
+a=5
+b=67
+def sum(a,b):
+    c=a+b
+    return c
+print("\n I HAVE ADDED YO: ",sum(a,b))
+
+def mltply(a,b):
+    c= a*b
+    return c
+print("\n I HAVE DONE MULTPLICATION TOO:  ", mltply(a,b))
