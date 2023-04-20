7707c4157ca6a5c224237f37b916945c34e014a5
Debasish-Rath93 <erdebasishrath1993@gmail.com>
2023-04-20 17:33:57 +0530

    mene kuch toh kiya h

--- data_process.py
+++ data_process.py
@@ -1,3 +1,12 @@
 
 print("i dont know what to do?")
 print("\n whover whatever my code my world")
+
+a=5
+b= 36
+
+def sum(a,b):
+    c= a+b
+    return c
+
+print("\n O did something very wrong kya ukhadoge")
