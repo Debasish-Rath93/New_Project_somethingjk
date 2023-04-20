f40a7fa351247f3f345ab9e9b28b87ace6bebd7f
Debasish-Rath93 <erdebasishrath1993@gmail.com>
2023-04-20 22:22:30 +0530

    added something more to the equaiton

--- data_process.py
+++ data_process.py
@@ -1,3 +1,8 @@
 
 print("i dont know what to do?")
 print("\n whover whatever my code my world")
+def mltply(a,b):
+    c = a*b
+    return c
+
+print("I GUESSS WE`ll go with multiply: ", mltply(a,b))
---
+++ smthng.py
@@ -0,0 +1,15 @@
+print("\n blah blah whover whatever my code my world")
+
+def mltply(a,b):
+    c = a*b
+    return c
+a=85
+b=36
+
+print("I GUESSS WE`ll go with multiply: ", mltply(a,b))
+
+def facto(a,b):
+    c= a*b*(a+b)/(a-b)
+    return c
+
+print("I GUESSS WE`ll go with expo: ", facto(a,b))
